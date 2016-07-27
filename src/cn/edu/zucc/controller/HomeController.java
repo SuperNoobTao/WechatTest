2be21f6c447aa77fa0c4374af845e43e07a9f275
shentao <387944558@qq.com>
2016-07-27 16:33:20 +0800

    Initial commit

---
+++ src/cn/edu/zucc/controller/HomeController.java
@@ -0,0 +1,47 @@
+package cn.edu.zucc.controller;
+
+import cn.edu.zucc.util.SignUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+/**
+ * Created by vito on 2016/7/26.
+ */
+@Controller
+@RequestMapping("/welcome")
+public class HomeController {
+
+
+
+    @RequestMapping(value="/api",method = RequestMethod.GET)
+    @ResponseBody
+    public void api( HttpServletResponse response, HttpServletRequest request) throws IOException {
+
+        // 微信加密签名
+        String signature = request.getParameter("signature");
+        // 时间戳
+        String timestamp = request.getParameter("timestamp");
+        // 随机数
+        String nonce = request.getParameter("nonce");
+        // 随机字符串
+        String echostr = request.getParameter("echostr");
+        PrintWriter out = response.getWriter();
+        // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
+        if (SignUtil.checkSignature(signature, timestamp, nonce)) {
+            out.print(echostr);
+        }
+        out.close();
+        out = null;
+
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: vito
+  Date: 2016/7/26
+  Time: 10:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>

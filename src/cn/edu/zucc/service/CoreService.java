17e447d51f3971094f5bc434c58133cd18017fe6
shentao <387944558@qq.com>
2016-07-28 13:59:04 +0800

    自定义菜单

--- src/cn/edu/zucc/service/CoreService.java
+++ src/cn/edu/zucc/service/CoreService.java
@@ -1,100 +1,98 @@
 package cn.edu.zucc.service;
 
-
-
+import cn.edu.zucc.message.resp.Article;
+import cn.edu.zucc.message.resp.NewsMessage;
 import cn.edu.zucc.message.resp.TextMessage;
 import cn.edu.zucc.util.MessageUtil;
-import cn.edu.zucc.util.XiaoqUtil;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+
 
 /**
- * Created by vito on 2016/7/25.
+ * 核心服务类
+ *
+ * @author liufeng
+ * @date 2013-10-17
  */
 public class CoreService {
     /**
      * 处理微信发来的请求
      *
      * @param request
-     * @return
+     * @return xml
      */
     public static String processRequest(HttpServletRequest request) {
-        String respMessage = null;
+        // xml格式的消息数据
+        String respXml = null;
         try {
-            // 默认返回的文本消息内容
-            String respContent = "请求处理异常，请稍候尝试！";
-
-            // xml请求解析
+            // 调用parseXml方法解析请求消息
             Map<String, String> requestMap = MessageUtil.parseXml(request);
-
-            // 发送方帐号（open_id）
+            // 发送方帐号
             String fromUserName = requestMap.get("FromUserName");
-            // 公众帐号
+            // 开发者微信号
             String toUserName = requestMap.get("ToUserName");
             // 消息类型
             String msgType = requestMap.get("MsgType");
 
-            // 回复文本消息
             TextMessage textMessage = new TextMessage();
             textMessage.setToUserName(fromUserName);
             textMessage.setFromUserName(toUserName);
             textMessage.setCreateTime(new Date().getTime());
             textMessage.setMsgType(MessageUtil.RESP_MESSAGE_TYPE_TEXT);
-            textMessage.setFuncFlag(0);
-
-            // 文本消息
-            if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_TEXT)) {
-                String content = requestMap.get("Content");
-                System.out.println(content);
-                // 判断用户发送的是否是单个QQ表情
-                if(XiaoqUtil.isQqFace(content)) {
-                    respContent = content;
-                }
-
-            }
-
-            // 图片消息
-            else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_IMAGE)) {
-                respContent = "您发送的是图片消息！";
-            }
-            // 地理位置消息
-            else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_LOCATION)) {
-                respContent = "您发送的是地理位置消息！";
-            }
-            // 链接消息
-            else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_LINK)) {
-                respContent = "您发送的是链接消息！";
-            }
-            // 音频消息
-            else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_VOICE)) {
-                respContent = "您发送的是音频消息！";
-            }
             // 事件推送
-            else if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_EVENT)) {
+            if (msgType.equals(MessageUtil.REQ_MESSAGE_TYPE_EVENT)) {
                 // 事件类型
                 String eventType = requestMap.get("Event");
                 // 订阅
                 if (eventType.equals(MessageUtil.EVENT_TYPE_SUBSCRIBE)) {
-                    respContent = "谢谢您的关注！";
+                    textMessage.setContent("您好，欢迎关注网址导航！我们致力于打造精品网址聚合应用，为用户提供便捷的上网导航服务。体验生活，从这里开始！");
+                    // 将消息对象转换成xml
+                    respXml = MessageUtil.textMessageToXml(textMessage);
                 }
                 // 取消订阅
                 else if (eventType.equals(MessageUtil.EVENT_TYPE_UNSUBSCRIBE)) {
-                    // TODO 取消订阅后用户再收不到公众号发送的消息，因此不需要回复消息
+                    // TODO 暂不做处理
                 }
                 // 自定义菜单点击事件
                 else if (eventType.equals(MessageUtil.EVENT_TYPE_CLICK)) {
-                    // TODO 自定义菜单权没有开放，暂不处理该类消息
+                    // 事件KEY值，与创建菜单时的key值对应
+                    String eventKey = requestMap.get("EventKey");
+                    // 根据key值判断用户点击的按钮
+                    if (eventKey.equals("oschina")) {
+                        Article article = new Article();
+                        article.setTitle("开源中国");
+                        article.setDescription("开源中国社区成立于2008年8月，是目前中国最大的开源技术社区。\n\n开源中国的目的是为中国的IT技术人员提供一个全面的、快捷更新的用来检索开源软件以及交流开源经验的平台。\n\n经过不断的改进,目前开源中国社区已经形成了由开源软件库、代码分享、资讯、讨论区和博客等几大频道内容。");
+                        article.setPicUrl("");
+                        article.setUrl("http://m.oschina.net");
+                        List<Article> articleList = new ArrayList<Article>();
+                        articleList.add(article);
+                        // 创建图文消息
+                        NewsMessage newsMessage = new NewsMessage();
+                        newsMessage.setToUserName(fromUserName);
+                        newsMessage.setFromUserName(toUserName);
+                        newsMessage.setCreateTime(new Date().getTime());
+                        newsMessage.setMsgType(MessageUtil.RESP_MESSAGE_TYPE_NEWS);
+                        newsMessage.setArticleCount(articleList.size());
+                        newsMessage.setArticles(articleList);
+                        respXml = MessageUtil.newsMessageToXml(newsMessage);
+                    } else if (eventKey.equals("iteye")) {
+                        textMessage.setContent("ITeye即创办于2003年9月的JavaEye,从最初的以讨论Java技术为主的技术论坛，已经逐渐发展成为涵盖整个软件开发领域的综合性网站。\n\nhttp://www.iteye.com");
+                        respXml = MessageUtil.textMessageToXml(textMessage);
+                    }
                 }
             }
-
-            textMessage.setContent(respContent);
-            respMessage = MessageUtil.textMessageToXml(textMessage);
+            // 当用户发消息时
+            else {
+                textMessage.setContent("请通过菜单使用网址导航服务！");
+                respXml = MessageUtil.textMessageToXml(textMessage);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        return respMessage;
+        return respXml;
     }
 }
